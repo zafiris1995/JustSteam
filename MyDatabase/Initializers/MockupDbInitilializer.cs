6c6a681c4fb5cdcbc387d9d5932eeb1d8b7f4173
KomhKs <101747946+KomhKs@users.noreply.github.com>
2022-09-12 11:55:08 +0300

    Update MockupDbInitilializer.cs
    added release date and photoUrl to games

--- MyDatabase/Initializers/MockupDbInitilializer.cs
+++ MyDatabase/Initializers/MockupDbInitilializer.cs
@@ -15,29 +15,29 @@ namespace MyDatabase.Initializers
         {
             #region Game Seeding
 
-            Game g1 = new Game() { Title = "Fifa", Price = 10 , Rating = 10 };
-            Game g2 = new Game() { Title = "Zelda", Price = 5, Rating = 3 };
-            Game g3 = new Game() { Title = "Tetris", Price = 3, Rating = 6 };
-            Game g4 = new Game() { Title = "Starcraft", Price = 10, Rating = 9 };
-            Game g5 = new Game() { Title = "Diablo", Price = 5, Rating = 5 };
-            Game g6 = new Game() { Title = "Mass Efect", Price = 20, Rating = 5 };
-            Game g7 = new Game() { Title = "Dragon Age: Origins", Price = 10, Rating = 10 };
-            Game g8 = new Game() { Title = "Half-Life 3", Price = 70, Rating = 1 };
-            Game g9 = new Game() { Title = "Age of Empires II", Price = 15, Rating = 3 };
-            Game g10 = new Game() { Title = "Starcraft II", Price = 20, Rating = 4 };
-            Game g11 = new Game() { Title = "Diablo II", Price = 10, Rating = 5 };
-            Game g12 = new Game() { Title = "Pathfinder: Kingmaker", Price = 30, Rating = 3 };
-            Game g13 = new Game() { Title = "Oxygen Not Included", Price = 25, Rating = 8 };
-            Game g14 = new Game() { Title = "Rimworld", Price = 30, Rating = 8 };
-            Game g15 = new Game() { Title = "Diablo III", Price = 15, Rating = 4 };
-            Game g16 = new Game() { Title = "Cities: Skylines", Price = 20, Rating = 6 };
-            Game g17 = new Game() { Title = "Final Fantasy VII", Price = 7, Rating = 3 };
-            Game g18 = new Game() { Title = "Resident Evil", Price = 9, Rating = 4 };
-            Game g19 = new Game() { Title = "Monkey Island", Price = 15, Rating = 10 };
-            Game g20 = new Game() { Title = "Diablo IV", Price = 60, Rating = 5 };
-            Game g21 = new Game() { Title = "MultiVersus", Price = 50, Rating = 7 };
+            Game g1 = new Game() { Title = "Fifa 23", Price = 10 , Rating = 10, ReleaseDate = new DateTime(2022/09/30), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/a/a6/FIFA_23_Cover.jpg" };
+            Game g2 = new Game() { Title = "The Legend of Zelda", Price = 5, Rating = 3, ReleaseDate = new DateTime(1986/02/21), PhotoUrl = "https://upload.wikimedia.org/wikipedia/el/d/d0/Zelda_Cover_Europe.jpg"  };
+            Game g3 = new Game() { Title = "Tetris", Price = 3, Rating = 6, ReleaseDate = new DateTime(1986/01/01), PhotoUrl = "https://upload.wikimedia.org/wikipedia/el/8/8d/NES_Tetris_Box_Front.jpg"   };
+            Game g4 = new Game() { Title = "Starcraft", Price = 10, Rating = 9, ReleaseDate = new DateTime(1998/03/31), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/9/93/StarCraft_box_art.jpg" };
+            Game g5 = new Game() { Title = "Diablo", Price = 5, Rating = 5, ReleaseDate = new DateTime(1997/01/03), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/3/3a/Diablo_Coverart.png"  };
+            Game g6 = new Game() { Title = "Mass Effect", Price = 20, Rating = 5, ReleaseDate = new DateTime(2007/11/20), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/e/e8/MassEffect.jpg"  };
+            Game g7 = new Game() { Title = "Dragon Age: Origins", Price = 10, Rating = 10, ReleaseDate = new DateTime(2009/11/03), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/8/89/Dragon_Age_Origins_cover.png"  };
+            Game g8 = new Game() { Title = "Half-Life", Price = 70, Rating = 1, ReleaseDate = new DateTime(1998/11/19), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/f/fa/Half-Life_Cover_Art.jpg"  };
+            Game g9 = new Game() { Title = "Age of Empires II", Price = 15, Rating = 3, ReleaseDate = new DateTime(1999/09/30), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/5/56/Age_of_Empires_II_-_The_Age_of_Kings_Coverart.png"  };
+            Game g10 = new Game() { Title = "Starcraft II", Price = 20, Rating = 4, ReleaseDate = new DateTime(2010/07/27), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/2/20/StarCraft_II_-_Box_Art.jpg"  };
+            Game g11 = new Game() { Title = "Diablo II", Price = 10, Rating = 5, ReleaseDate = new DateTime(2000/06/30), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png"  };
+            Game g12 = new Game() { Title = "Pathfinder: Kingmaker", Price = 30, Rating = 3, ReleaseDate = new DateTime(2018/09/25), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/e/e6/Pathfinder_Kingmaker_cover_art.jpg"  };
+            Game g13 = new Game() { Title = "Oxygen Not Included", Price = 25, Rating = 8, ReleaseDate = new DateTime(2019/07/30), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/f/f6/Oxygen_Not_Included_cover_art.jpg"  };
+            Game g14 = new Game() { Title = "Minecraft", Price = 30, Rating = 8, ReleaseDate = new DateTime(2009/05/17), PhotoUrl = "https://upload.wikimedia.org/wikipedia/el/8/82/Minecraft_cover.jpg"  };
+            Game g15 = new Game() { Title = "Diablo III", Price = 15, Rating = 4, ReleaseDate = new DateTime(2012/05/15), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/8/80/Diablo_III_cover.png"  };
+            Game g16 = new Game() { Title = "Cities: Skylines", Price = 20, Rating = 6, ReleaseDate = new DateTime(2015/03/05), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/5/58/Cities_Skylines_cover_art.jpg"  };
+            Game g17 = new Game() { Title = "Final Fantasy VII", Price = 7, Rating = 3, ReleaseDate = new DateTime(1997/01/30), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg"  };
+            Game g18 = new Game() { Title = "Resident Evil", Price = 9, Rating = 4, ReleaseDate = new DateTime(1996/03/22), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/a/a6/Resident_Evil_1_cover.png"  };
+            Game g19 = new Game() { Title = "The Secret of Monkey Island", Price = 15, Rating = 10, ReleaseDate = new DateTime(1990/10/01), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/a/a8/The_Secret_of_Monkey_Island_artwork.jpg"  };
+            Game g20 = new Game() { Title = "DotA 2", Price = 60, Rating = 5, ReleaseDate = new DateTime(2013/07/09), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/0/0b/Dota_2_%28Steam_2019%29.jpg"  };
+            Game g21 = new Game() { Title = "MultiVersus", Price = 50, Rating = 7, ReleaseDate = new DateTime(2022/08/15), PhotoUrl = "https://upload.wikimedia.org/wikipedia/en/1/1d/MultiVersus_Cover_Art.png"  };
 
-            context.Games.AddOrUpdate(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20);
+            context.Games.AddOrUpdate(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20, g21);
             context.SaveChanges();
 
             #endregion
