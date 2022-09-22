81e9f252038035c95a5209b529fea5daf901e9a0
zafiris argiros <zafiris.argiros69@outlook.com>
2022-09-22 16:41:54 +0300

    decorator required

--- Entities/Game.cs
+++ Entities/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Entities
     public class Game
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public int Price { get; set; }
         public int Rating { get; set; }
